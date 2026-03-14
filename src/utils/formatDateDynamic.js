660326705683b27932e2bc3e9e3dea9e65fd6681
devsujoydas <devsujoydas@gmail.com>
2026-03-14 15:31:56 +0600

    upate components

--- src/utils/formatDateDynamic.js
+++ src/utils/formatDateDynamic.js
@@ -1,5 +1,7 @@
 export const formatDateDynamic = (dateStr) => {
     const currentLang = document.documentElement.lang;
+    console.log(currentLang)
+
     const d = new Date(dateStr);
     return d.toLocaleDateString(currentLang, {
         day: "numeric",
